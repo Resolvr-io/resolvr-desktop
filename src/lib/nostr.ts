ed6ed5957c91c7c3516ccc3c1e103fdf68290c13
Bri <brianjcarroll0@gmail.com>
2024-02-20 15:53:21 -0500

    address pr comments

--- src/lib/nostr.ts
+++ src/lib/nostr.ts
@@ -1,6 +1,19 @@
 import { invoke } from "@tauri-apps/api/tauri";
 import { nip19 } from "nostr-tools";
 
+interface Window {
+  nostr: Nostr;
+}
+
+// https://github.com/nostr-protocol/nips/blob/master/07.md
+interface Nostr {
+  getPublicKey(): Promise<unknown>;
+  signEvent(event: unknown): Promise<unknown>;
+}
+
+declare const nostr: Nostr;
+declare const window: Window;
+
 window.nostr = {
   getPublicKey: async function () {
     const publicKey: `npub1${string}` = await invoke("login", {});
